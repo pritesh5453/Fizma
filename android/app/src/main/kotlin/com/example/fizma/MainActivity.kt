a68f9ab98aeaef7b1620bb1cbb8e5de06a26fc7b
pritesh5453 <pritesh5453@gmail.com>
2026-07-07 14:33:09 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fizma/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fizma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:fizma/Screens/onboarding/splash_screen1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fizma',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen2(),
+    );
+  }
+}
